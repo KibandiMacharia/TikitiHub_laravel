c587cdb1f22d8b9e32b803349bd3083450a763c8
Deon254 <54399677+Deon254@users.noreply.github.com>
2020-09-07 18:22:21 +0300

    Movie Schedule Controller

---
+++ app/Http/Controllers/ScheduleController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Schedules;
+
+class ScheduleController extends Controller
+{
+    public function home(){
+            $schedule = Schedules::all();
+            return view('pages.admin.admin_movie_schedule',['schedule' => $schedule]);
+          }
+
+
+     public function destroy(Schedules $schedule) {
+        $schedule->delete();
+        return redirect()->route('schedule')
+                        ->with(['failure','Schedule deleted successfully']);
+    }
+
+}
+
+
+
+
