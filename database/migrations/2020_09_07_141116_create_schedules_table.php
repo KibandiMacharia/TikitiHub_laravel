1ab633f1f57674946c331761e539a83674c160a4
Deon254 <54399677+Deon254@users.noreply.github.com>
2020-09-07 18:23:20 +0300

    Movie Schedule migration

---
+++ database/migrations/2020_09_07_141116_create_schedules_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSchedulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schedules', function (Blueprint $table) {
+              $table->bigIncrements('id');
+            $table->string('movie_title');
+          $table->string('cinema_name');
+          $table->date('date');
+           $table->time("time");
+           $table->integer('ticket_cost');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schedules');
+    }
+}
