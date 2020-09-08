e900f842c8ea69a1cf86dc14c0543f62c60f1e8f
Deon254 <54399677+Deon254@users.noreply.github.com>
2020-09-08 23:51:09 +0300

    Movie info model

---
+++ app/Movie.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Movie extends Model
+{
+	protected $table= 'movie_info' ;
+
+     protected $fillable = [
+        'movie_title', 'movie_cast','movie_genre','movie_description','movie_poster','url','release_year','language',
+      ];
+        protected $primaryKey = 'movie_id';
+    }
