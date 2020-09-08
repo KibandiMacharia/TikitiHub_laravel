38152c271f4579dd4394b22f68b7e4c9450aed80
Deon254 <54399677+Deon254@users.noreply.github.com>
2020-09-08 23:51:51 +0300

    Movie info migration

---
+++ database/migrations/2020_09_03_104343_create_movie_info_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMovieInfoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movie_info', function (Blueprint $table) {
+            $table->bigIncrements('movie_id');
+            $table->string('movie_title');
+            $table->string('movie_cast');
+            $table->string('movie_genre');
+            $table->text('movie_description');
+            $table->string('movie_poster');
+            $table->string('url');
+            $table->integer('release_year');
+            $table->string('language');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movie_info');
+    }
+}
